50d71ea76b321ba7d3dd723d355ca3d21d026c60
niranjan-gurung <gurung5130@gmail.com>
2025-04-21 21:19:29 +0100

    piece info extraction -> started building armour piece model

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
 
 """
-Starting to work... kinda
-"""
-
-"""
-this block goes from homepage to armour page
+homepage -> armour page
 """
 data = soup.find(attrs={'data-sidebar': 'group-content'})
 a = data.find_all('a')
@@ -26,77 +22,91 @@ for item in a:
 
 
 """
-this block goes from armour page to Hope armour page.
+armour page -> first armour page (Hope armour).
 Each individual armour page has link to next or previous armour.
 """
-#armour_names = []
-
 first_armour_link = new_soup.find('tr') \
                             .find('td') \
                             .find('a')
 
 if first_armour_link.text == 'Hope' and 'href' in first_armour_link.attrs:
-    href = first_armour_link['href']
-    armour_url = requests.compat.urljoin(url, href)
-    new_res = requests.get(armour_url)
-    new_soup = BeautifulSoup(new_res.text, 'html.parser')
+  href = first_armour_link['href']
+  armour_url = requests.compat.urljoin(url, href)
+  new_res = requests.get(armour_url)
+  new_soup = BeautifulSoup(new_res.text, 'html.parser')
 
 tables = new_soup.find_all('tbody')
-t1 = tables[0]    # piece names
+#t1 = tables[0]    # piece names
 t2 = tables[1]    # piece type + resistances
 t3 = tables[2]    # piece slot info
 
-"""
-removes any excess words connected to 2nd word (starting with uppercase).
-ex. 'Hope MaskArmour' -> 'Hope Mask'
-One of the necessary fields for ArmourModel + Schema.
-"""
-armour_pieces = re.findall(r'Hope [A-Z][a-z]+(?=[A-Z])?', t1.text)
-# print(armour_pieces)
+# """
+# removes any excess words connected to 2nd word (starting with uppercase).
+# ex. 'Hope MaskArmour' -> 'Hope Mask'
+# One of the necessary fields for ArmourModel + Schema.
+# """
+#armour_pieces = re.findall(r'Hope [A-Z][a-z]+(?=[A-Z])?', t1.text)
+#print(armour_pieces)
 
+
+"""
+uses 2nd and 3rd table structures to extract appropriate data:
+- name
+- type
+- defense
+- resistances
+- slots
+
+builds a armour piece dict object using the above info
+"""
+data = []
+rows = t2.find_all('tr')[1:]
+
+for row in rows:
+  td = row.find_all('td')
+  type = td[0].get_text(strip=True)
+  name = td[1].get_text(strip=True)
+  defense = td[2].get_text(strip=True)
+  resistances = {
+    "fire": td[3].get_text(strip=True),
+    "water": td[4].get_text(strip=True),
+    "ice": td[5].get_text(strip=True),
+    "thunder": td[6].get_text(strip=True),
+    "dragon": td[7].get_text(strip=True)
+  }
+
+  """
+  this block extracts slots information
+  """
+  rows = t3.find_all('tr')[1:]
+  for row in rows:
+    td = row.find_all('td')
+    slot_row = td[2].get_text(strip=True) \
+                    .strip('[]')          \
+                    .split('][')
+    slots = [{'level': int(slot)} for slot in slot_row if int(slot) != 0]
+
+  data.append({
+    'name': name,
+    'slug': name.lower().replace(' ', '-'),
+    'type': type.lower(),
+    'defense': defense,
+    'resistances': resistances,
+    'slots': slots
+  })
+
+print(data)
 
 """
 Link to next armour set
 """
-next_armour = new_soup.find(name='nav', attrs={'role': 'navigation'}) \
-                      .find('ul') \
-                      .find_all('li')[-1].find('a')
-href = next_armour['href']
-next_armour_url = requests.compat.urljoin(url, href)
-new_res = requests.get(next_armour_url)
-new_soup = BeautifulSoup(new_res.text, 'html.parser')
+# next_armour = new_soup.find(name='nav', attrs={'role': 'navigation'}) \
+#                       .find('ul') \
+#                       .find_all('li')[-1].find('a')
+# href = next_armour['href']
+# next_armour_url = requests.compat.urljoin(url, href)
+# new_res = requests.get(next_armour_url)
+# new_soup = BeautifulSoup(new_res.text, 'html.parser')
 
-h = new_soup.find('h2')
-print(h)
-
-"""
-old way... redundant now: 
-"""
-#print(tr_list.text)
-# a = tr_list.
-# print(a)
-# for item in tr_list:
-#   armour_links = item.find_all('a')
-
-#   """
-#   remove female version as we only want name of armour set.
-#   stats, res, def, skills are basically duplicate of male ver.
-#   """
-#   if item.text != '':
-#     armour_name.append(item.text)
-
-#   for armour in armour_links:
-#     if armour.text == 'Hope' and 'href' in armour.attrs:
-#       href = armour['href']
-#       print(href)
-#       armour_url = requests.compat.urljoin(url, href)
-#       print(armour_url)
-#       new_res = requests.get(armour_url)
-#       print(new_res)
-#       new_soup = BeautifulSoup(new_res.text, 'html.parser')
-#       break
-
-# t = new_soup.find('h2')
-# print(t)
-
-
+# h = new_soup.find('h2')
+# print(h)
